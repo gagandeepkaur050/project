db70e64e8efc31273ed8983b677cf9ebf99b4a6e
gagandeep <kaurgagandeep656@gmail.com>
2024-11-20 16:09:38 -0500

    Made changes to main activity

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.example.project
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.project.databinding.ActivityMainBinding
+import com.example.project.R
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var textView: TextView
+    private lateinit var editText: EditText
+    private lateinit var button: Button
 
-    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        // Initialize UI elements
+        textView = findViewById(R.id.textView)
+        editText = findViewById(R.id.editText)
+        button = findViewById(R.id.button)
+    }
+
+    // Method called when the button is clicked
+    fun onSubmitClicked(view: View) {
+        val userInput = editText.text.toString() // Get the text from EditText
+        textView.text = "Hello, $userInput!" // Update the TextView
     }
 }
