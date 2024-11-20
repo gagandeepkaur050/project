bb89d02d6964d3783171b4ccdb3496ef00d0883d
gagandeep <kaurgagandeep656@gmail.com>
2024-11-20 16:01:19 -0500

    Made changes to main activity

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -11,8 +11,6 @@ class MainActivity : AppCompatActivity() {
 
 
     lateinit var binding:ActivityMainBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
