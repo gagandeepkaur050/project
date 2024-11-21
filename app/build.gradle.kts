810991c47adaa310f8869757553fff111f2a08d6
gagandeep <kaurgagandeep656@gmail.com>
2024-11-21 12:23:24 -0500

    Made changes to main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,6 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
-    implementation("com.google.code.gson:gson:2.9.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 
 }
